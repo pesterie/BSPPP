05b9132d59b0f65dd695e503fe62ece68d6adfc8
pesterie <pesterie@gmail.com>
2011-05-10 14:54:53 +0200

    Cleaning main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -21,44 +21,34 @@ int bsp_main(int argc, char** argv);
 
 #include <boost/mpi.hpp>
 
-//namespace bsp
-//{
 int main (int argc, char** argv)
 {
-/// start the MPI env
+  /// start the MPI env
 
-if(!MPI::bsp::details::runtime::running)
-        {
-          MPI::bsp::details::runtime::environment = new boost::mpi::environment(argc,argv);
-          MPI::bsp::details::runtime::rank        = MPI::bsp::details::runtime::world.rank();
-          MPI::bsp::details::runtime::procs       = MPI::bsp::details::runtime::world.size();
-          MPI::bsp::details::runtime::running     = true;
-        }
-
-/// call to the user main function
-
-bsp_main(argc, argv);
-
-
-/// Finalize the MPI env
+  if(!MPI::bsp::details::runtime::running)
+    {
+      MPI::bsp::details::runtime::environment = new boost::mpi::environment(argc,argv);
+      MPI::bsp::details::runtime::rank        = MPI::bsp::details::runtime::world.rank();
+      MPI::bsp::details::runtime::procs       = MPI::bsp::details::runtime::world.size();
+      MPI::bsp::details::runtime::running     = true;
+    }
+  
+  /// call to the user main function
+  
+  bsp_main(argc, argv);
 
 
-if(MPI::bsp::details::runtime::environment) delete MPI::bsp::details::runtime::environment;
-MPI::bsp::details::runtime::environment = 0;
+  /// Finalize the MPI env
 
-
-
-
-//}
-
-
+  
+  if(MPI::bsp::details::runtime::environment) delete MPI::bsp::details::runtime::environment;
+  MPI::bsp::details::runtime::environment = 0;
+  
 }
 
 #endif
 
 #if defined(BSP_OMP_TARGET)
-
-
 
 /******************************************************************************
  * OMP Parallel environnement
@@ -66,29 +56,17 @@ MPI::bsp::details::runtime::environment = 0;
 
 #include <omp.h>
 #include <boost/program_options.hpp>
-//namespace bsp
-//{
+
 int main (int argc, char** argv)
 {
-//using namespace OMP::bsp;
 
-/// start the OMP parallel section
+  /// start the OMP parallel section
 #pragma omp parallel
-{
-
-/// call to the user main function
-bsp_main(argc, argv);
-
-
-/// Finalize the MPI env
-
+  {
+  /// call to the user main function
+  bsp_main(argc, argv);
+  /// Finalize the MPI env
+  }
 }
-
-//}
-
-
-}
-
-
 
 #endif
